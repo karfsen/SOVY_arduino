4198aadd83c579e486366bbcde55017106d97e5c
karfsen <martin.krendzelak@akademiasovy.sk>
2019-02-20 23:52:05 +0100

    20.02.2018serverupdate

---
+++ project/script4data/script.js
@@ -0,0 +1,70 @@
+const http=require('http');
+const express = require('express');
+const mysql = require('mysql');
+
+waitAndDo();
+
+/*function getTotal(callback){
+    
+    const con=mysql.createConnection({
+        host: "localhost",
+        user: "pedometer",
+        password: "160518",
+        database: "pedometer",
+        port: "3307"
+    });
+
+    con.connect((err)=>{
+        if (err) throw err;
+        let sql="select max(totalSteps) as totalSteps from data "+
+        "INNER JOIN users on data.id=users.id "+ 
+        "where users.id=1;";
+
+        con.query(sql,(err,res)=>{
+            if (err) throw err;
+            console.log(res);
+            value=res[0].totalSteps;
+            console.log(value);
+        });
+        callback(value);
+    });
+    con.end();
+}
+*/
+
+let totalSteps=19128;
+function waitAndDo(times/*,totalSteps*/) {
+    if(times < 1) {
+      return;
+    }
+    setTimeout(()=> {
+
+        const con=mysql.createConnection({
+            host: "localhost",
+            user: "pedometer",
+            password: "160518",
+            database: "pedometer",
+            port: "3307"
+        });
+
+        let id=1;
+        let steps=Math.floor(Math.random() * Math.floor(0));
+        console.log(steps);
+        con.connect((err)=>{
+            if (err) throw err;      
+            console.log("connected");
+            totalSteps=totalSteps+steps;
+            let sql="INSERT INTO data(id,thisSessionSteps,totalSteps) "+
+            "VALUES("+id+","+steps+","+totalSteps+");";
+            console.log(sql);
+            con.query(sql,(err,res)=>{
+                if (err) throw err;
+                console.log("Data inserted into database:"+id+" "+steps+" "+totalSteps);
+            });
+            con.end();
+        });
+    waitAndDo(times+1);
+    }, 
+    60000);
+  }
+
--- project/server/server.js
+++ project/server/server.js
@@ -2,11 +2,22 @@ const http=require('http');
 const express = require('express');
 const mysql = require('mysql');
 const TokenGenerator = require('uuid-token-generator');
-const screen=require('screen');
 let token;
 
 
-var con = mysql.createConnection({
+
+
+const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
+var app=express();
+app.use(express.json());
+
+app.post('/register',(req,res,callback)=>{
+
+  callback=function(value){
+    res.status(200).send(value);
+  };
+
+const con=mysql.createConnection({
     host: "localhost",
     user: "pedometer",
     password: "160518",
@@ -14,104 +25,90 @@ var con = mysql.createConnection({
     port: "3307"
 });
 
-const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
-var app=express();
-app.use(express.json());
-
-app.post('/register',(req,res)=>{
-    //console.log(req.body);
-    //console.log(req.body.name);
-    // console.log(req.body.pass);
-    // console.log(req.body.arduinoID);
-
     con.connect((err)=>{
-      if (err) {
-        console.log(err+"   at line 29"); 
-        res.status(500).send(err);
-      }
-      else{
+      if (err) throw err;      
         console.log("connected");
-        let sql = "INSERT INTO `users`(`username`,`password`, `arduinoID`) VALUES ('"+req.body.name+"','"+req.body.pass+"','"+req.body.arduinoid+"')";
-        con.query(sql,(err)=>{
-        if (err) {
-          res.status(400).send(err);
-          console.log(err+"   at line 37");
-        }
-        else{
-          console.log("NEW USER! name:"+req.body.name+"password"+req.body.pass+"arduino id :"+req.body.arduinoid);
-          res.status(201).send("Successfully registered!");
-          }
-        });
-      }
+        let sql = "INSERT INTO users(username,password,arduinoID) "+
+        "VALUES ('"+req.body.name+"','"+req.body.password+"','"+req.body.arduinoid+"');";
+        console.log(sql);
+        con.query(sql,(err,res)=>{
+          if (err) throw err;
+          console.log("NEW USER! name: "+req.body.name+" password:"+req.body.pass+" arduino id : "+req.body.arduinoid);
     });
-});
-
-app.post('/login',(req,res)=>{
-  let data;
-  let name=req.body.name;
-  let pw=req.body.pass;
-    //console.log(req.body.meno);
-    //console.log(req.body.pass);
-    //console.log(JSON.stringify(data));
-  con.connect((err)=>{
-    if (err){
-    console.log(err+"at line 58");
-    res.status(500).send();
-    }
-    else{
-      con.query("SELECT * FROM users",(err,result)=>{
-        data=result;
-      if (err) {
-        console.log(err+"at line 64");
-        res.status(500).send();
-      }
-      else{
-        for(let i=0;i<result.length;i++){
-          if((name===data[i].name)&&(pw===data[i].password)){
-            console.log(data[i].name,data[i].password);
-            res.status(200).send("Logged in!");
-          }
-          else{
-            console.log("Wrong username or pw");
-            res.status(401).send("Wrong username or password!");
-          }
-        }
-      }
-    });  
-  }  
-    
-    /*for(i=0;i<data.length;i++){
-      if((name==data.name[i])&&(pw==data.pass[i])){
-          token=tokgen.generate();
-          res.status(200).send(JSON.stringify(name,token));
-      }
-    }  */
+    callback("New user: "+req.body.name+" Successfully registered!");
   });
 });
 
+app.post('/login',(req,res,callback)=>{
+  callback=function(value){
+    res.status(200).send(value);
+  };
+
+  const con=mysql.createConnection({
+    host: "localhost",
+    user: "pedometer",
+    password: "160518",
+    database: "pedometer",
+    port: "3307"
+});
+
+  let data;
+  let name=req.body.name;
+  let pw=req.body.password;
+  let json;
+    //console.log(req.body.meno);
+    //console.log(req.body.pass);
+    //console.log(JSON.stringify(data));
+    con.connect((err)=>{
+      if (err) throw err;      
+      console.log("connected");
+      let sql="SELECT username,password FROM users "+
+      "WHERE username like '"+name+"' "+
+      "and password like '"+pw+"';";
+      console.log(sql);
+      con.query(sql,(err,res)=>{
+        if(err) throw err;
+        if(res.length==0){
+          console.log("User: "+name+" with password: "+pw+" is not in database");
+          callback("Incorrect Username or password!");
+        }
+        else{
+          let obj=new Object();
+          console.log("User: "+name+" with password: "+pw+" is in database");
+          obj.username=res[0].username;
+          obj.password=res[0].password;
+          callback(JSON.stringify(obj));
+        }
+    });
+  });
+});
 app.post('/getsteps',(req,res)=>{
+  
+  callback=function(value){
+    res.status(200).send(value);
+  };
+
+  const con=mysql.createConnection({
+    host: "localhost",
+    user: "pedometer",
+    password: "160518",
+    database: "pedometer",
+    port: "3307"
+});
+
   let name=req.body.name;
   con.connect((err)=>{
-    if (err){ 
-      console.log(err+"at line 92");
-      res.status(500).send();
-    }
-    else{
-      id=con.query("select id from users where username like '"+name+"';");
-      con.query("SELECT * FROM data where id like "+id+";",(err,result)=>{
-        if (err){ 
-          console.log(err+"at line 97");
-          res.status(401).send();
-        }
-          
-        else{
-          for(let i=0;i<result.length;i++){
-            console.log(result);
-            res.status(200).send(result);
-          }
-        }
+    if (err) throw err;
+      let sql="select time,thisSessionSteps,totalSteps from data "+
+      "INNER JOIN users on data.id=users.id "+ 
+      "where username like '"+name+"';";
+
+      con.query(sql,(err,res)=>{
+        if (err) throw err;
+        
+        console.log(res);
+        callback((res));
       });
-    } 
   });
 });
 
