2ccfdc56d6e857e51122651769db4d928c84bd5c
karfsen <martin.krendzelak@akademiasovy.sk>
2019-02-21 21:00:08 +0100

    Changes to server with comments

--- project/server/server.js
+++ project/server/server.js
@@ -1,18 +1,23 @@
+//zadefinovanie ake knižnice používame
 const http=require('http');
 const express = require('express');
 const mysql = require('mysql');
 const TokenGenerator = require('uuid-token-generator');
-let token;
 
 
 
-
-const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
+//Definuje token generaciu(pomocou knižnice na tokeny)
+const tokgen = new TokenGenerator(128, TokenGenerator.BASE62);
+console.log(tokgen);
 var app=express();
 app.use(express.json());
 
+//Tu je registracia užívateľa , je to viacmenej na 100% hotove
+
 app.post('/register',(req,res,callback)=>{
 
+//callback asynchronnej funkcie
+  
   callback=function(value){
     res.status(200).send(value);
   };
@@ -34,12 +39,36 @@ const con=mysql.createConnection({
         con.query(sql,(err,res)=>{
           if (err) throw err;
           console.log("NEW USER! name: "+req.body.name+" password:"+req.body.pass+" arduino id : "+req.body.arduinoid);
-    });
+          });
+        con.end();
     callback("New user: "+req.body.name+" Successfully registered!");
   });
 });
+ 
+//Toto je funkcia ktoru som vytvoril na vkladanie toho tokenu , pretože som nevedel ako to inak urobiť.proste než sa stihol token vygenerovať tak to prešlo sem a nechcelo to isť.
+//Asi to bude trebalo zas nejak cez tú callback funkciu urobiť
+
+function insertToken(token){
+  if(token==""){
+    console.log("cannot insert nothing!");
+  }
+  else{
+    con.connect((err)=>{
+      if(err) throw err;
+      console.log("tokenConnected!");
+      let sql="INSERT INTO tokens values("+id+",'"+token+"');";
+      con.query(sql,(err,res)=>{
+        if (err) throw err;
+        console.log("Token for user " +name+ " is: " +token);
+      });
+    });
+  }
+}
+
+//Login , 70%hotove (ešte tokeny a bude 100%)
 
 app.post('/login',(req,res,callback)=>{
+  
   callback=function(value){
     res.status(200).send(value);
   };
@@ -51,18 +80,17 @@ app.post('/login',(req,res,callback)=>{
     database: "pedometer",
     port: "3307"
 });
-
-  let data;
+  let id;
   let name=req.body.name;
   let pw=req.body.password;
-  let json;
+  let token="";
     //console.log(req.body.meno);
     //console.log(req.body.pass);
     //console.log(JSON.stringify(data));
     con.connect((err)=>{
       if (err) throw err;      
       console.log("connected");
-      let sql="SELECT username,password FROM users "+
+      let sql="SELECT id,username,password FROM users "+
       "WHERE username like '"+name+"' "+
       "and password like '"+pw+"';";
       console.log(sql);
@@ -73,15 +101,23 @@ app.post('/login',(req,res,callback)=>{
           callback("Incorrect Username or password!");
         }
         else{
-          let obj=new Object();
           console.log("User: "+name+" with password: "+pw+" is in database");
+          id=res.id;
+          token=tokgen.generate();
+          console.log(token);
+          let obj=new Object();
           obj.username=res[0].username;
-          obj.password=res[0].password;
+          obj.token=token;
           callback(JSON.stringify(obj));
         }
-    });
-  });
+      });
+      con.end();
+    }); 
 });
+
+
+//request na vyťahovanie dat z databazy , response príde zatiaľ len na request s menom , neskôr po dorobeni token systemu to pôjde cez meno a token
+
 app.post('/getsteps',(req,res)=>{
   
   callback=function(value){
@@ -109,9 +145,11 @@ app.post('/getsteps',(req,res)=>{
         console.log(res);
         callback((res));
       });
+      con.end();
   });
 });
 
+//Zadefinovanie portu servera.
 
 app.listen(1203,()=>{
     console.log("Sever listening on port 1203");
