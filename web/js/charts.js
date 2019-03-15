692ae42a17f2615ab23df4d234012946f1153816
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2019-03-15 01:08:00 +0100

    rozlozenie na page Statistics

--- web/js/charts.js
+++ web/js/charts.js
@@ -15,7 +15,7 @@
             console.log("jd: " + json_data);
             for (var i = 0; i < json_data.length; i++) {
                 var obj2 = json_data[i];
-                waterIntage.push(obj2.todaysteps);
+                steps.push(obj2.todaysteps);
                 times.push(i + 1);
             }
             console.log("json:" + json_data);
@@ -68,9 +68,8 @@
 
     //Nakreslenie grafu pre water intake usera; zatial napojene na fake api lebo api neide
     var xhttpWather = new XMLHttpRequest();
-
-
     xhttpWather.onreadystatechange = function () {
+
         if (this.readyState == 4 && this.status == 200) {
             //stiahne sa JSON z fake apy a vyberie sa z neho chodenie. to sa pouzilo na water inteke lebo este nie je api
             var json_data = JSON.parse(xhttpWather.responseText);
@@ -78,11 +77,18 @@
             let times = [];
             for (var i = 0; i < json_data.length; i++) {
                 var obj2 = json_data[i];
+
+                console.log(obj2);
                 waterIntage.push(obj2.mlOfWater);
-                times.push(i + 1);
+                let whole = obj2.time.split(' ');
+                let d = whole[0].split(".");
+                let t = whole[1].split(":");
+                let date = new Date(d[2], d[1], d[0], t[0], [1], 0, 0);
+                times.push(date.toDateString() + " " + date.toLocaleTimeString());
             }
+
             var ctx = document.getElementById("lineWater"); // vlozenie grafu do id lineDaily
-            ctx.height = 150;
+            ctx.height = 250;
 
             var myChart = new Chart(ctx, {
                 type: 'line',
@@ -103,7 +109,8 @@
                     ]
                 },
                 options: {
-                    responsive: true,
+                    // responsive: true,
+                    // maintainAspectRatio: false,
                     tooltips: {
                         mode: 'index',
                         intersect: false
@@ -126,65 +133,65 @@
 
 
     //nakreslenie grafu na prejdene kroky za rok; na pevno napisane cisla krokov lebo cakam na api
-
-    let months = [];
-    let stepsPerMonth = [];
-    let xhttp2 = new XMLHttpRequest();
-
-    xhttp2.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            // Action to be performed when the document is read;
-            // console.log(xhttp.responseText);
-            var json_data = JSON.parse(xhttp2.responseText);
-            Object.keys(json_data).forEach(function (key) {
-                months.push(key.toString());
-                stepsPerMonth.push(json_data[key]);
-                // console.log('Key : ' + key + ', Value : ' + json_data[key])
-            })
-            var result = [];
-            let arr = ["asd", "as"];
-            arr.push("asd")
-            // console.log(months);
-            // console.log(stepsPerMonth);
-            // console.log(arr);
-            var ctx = document.getElementById("lineYear");
-            ctx.height = 150;
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    // labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-                    // labels: arr,
-                    labels: months,
-                    datasets: [
-                        {
-                            label: "speps per month",
-                            borderColor: "rgba(0, 194, 146, 0.9)",
-                            borderWidth: "1",
-                            backgroundColor: "rgba(0, 194, 146,0.1)",
-                            // data: [20, 25, 15, 35, 43, 65, 45, 35, 50]
-                            data: stepsPerMonth
-                        },
-                    ]
-                },
-                options: {
-                    responsive: true,
-                    tooltips: {
-                        mode: 'index',
-                        intersect: false
-                    },
-                    hover: {
-                        mode: 'nearest',
-                        intersect: true
-                    }
-
-                }
-            });
-
-
-        }
-    };
-    xhttp2.open("GET", "https://my-json-server.typicode.com/oleksandra1musatkina/api/graf", true);
-    xhttp2.send();
+    //
+    // let months = [];
+    // let stepsPerMonth = [];
+    // let xhttp2 = new XMLHttpRequest();
+    //
+    // xhttp2.onreadystatechange = function () {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         // Action to be performed when the document is read;
+    //         // console.log(xhttp.responseText);
+    //         var json_data = JSON.parse(xhttp2.responseText);
+    //         Object.keys(json_data).forEach(function (key) {
+    //             months.push(key.toString());
+    //             stepsPerMonth.push(json_data[key]);
+    //             // console.log('Key : ' + key + ', Value : ' + json_data[key])
+    //         })
+    //         var result = [];
+    //         let arr = ["asd", "as"];
+    //         arr.push("asd")
+    //         // console.log(months);
+    //         // console.log(stepsPerMonth);
+    //         // console.log(arr);
+    //         var ctx = document.getElementById("lineYear");
+    //         ctx.height = 150;
+    //         var myChart = new Chart(ctx, {
+    //             type: 'line',
+    //             data: {
+    //                 // labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    //                 // labels: arr,
+    //                 labels: months,
+    //                 datasets: [
+    //                     {
+    //                         label: "speps per month",
+    //                         borderColor: "rgba(0, 194, 146, 0.9)",
+    //                         borderWidth: "1",
+    //                         backgroundColor: "rgba(0, 194, 146,0.1)",
+    //                         // data: [20, 25, 15, 35, 43, 65, 45, 35, 50]
+    //                         data: stepsPerMonth
+    //                     },
+    //                 ]
+    //             },
+    //             options: {
+    //                 responsive: true,
+    //                 tooltips: {
+    //                     mode: 'index',
+    //                     intersect: false
+    //                 },
+    //                 hover: {
+    //                     mode: 'nearest',
+    //                     intersect: true
+    //                 }
+    //
+    //             }
+    //         });
+    //
+    //
+    //     }
+    // };
+    // xhttp2.open("GET", "https://my-json-server.typicode.com/oleksandra1musatkina/api/graf", true);
+    // xhttp2.send();
 
 
     //Kreslenie remaining steps. Ked pojde api nakopirujem tu ten isty graf z daily goals
