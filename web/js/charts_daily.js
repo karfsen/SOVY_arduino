9761cac19dac418ac6f723b52f9e0cfef9e131ca
oleksandra1musatkina <43454012+oleksandra1musatkina@users.noreply.github.com>
2019-03-17 01:32:01 +0100

    uprava dailz goals

--- web/js/charts_daily.js
+++ web/js/charts_daily.js
@@ -1,54 +1,8 @@
 (function ($) {
     "use strict";
 
-    //nakreslenie grafu pre pocet splnenych cielov
-    var xhttp = new XMLHttpRequest();
-    let not_done = [];
-    let done = [];
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var json_data = JSON.parse(xhttp.responseText);
-            done = json_data["done"];
-            not_done = json_data["not_done"];
-
-            var result = [];
-
-
-            var ctx = document.getElementById("doughutGoals");
-            ctx.height = 250;
-            var myChart = new Chart(ctx, {
-                type: 'doughnut',
-                data: {
-                    datasets: [{
-                        data: [done, not_done],
-                        backgroundColor: [
-                            "rgba(0, 194, 146,0.9)",
-                            "rgba(194, 0, 0,0.7)",
-                        ],
-                        hoverBackgroundColor: [
-                            "rgba(0, 194, 146,0.7)",
-                            "rgba(194, 0, 0,0.7)",
-                        ]
-
-                    }],
-                    labels: [
-                        "Completed goals",
-                        "Remaining goals"
-                    ]
-                },
-                options: {
-                    responsive: true
-                }
-            });
-
-
-        }
-    };
-    xhttp.open("GET", "https://my-json-server.typicode.com/oleksandra1musatkina/api/daily_goals", true);
-    xhttp.send();
-
 //nakreslenie grafu pre splnenie goalu prejdenych krokov
-    let req = "http://itsovy.sk:1203/alltodaysteps";
+    let req = "http://itsovy.sk:1203/getsteps";
     var xhttp4 = new XMLHttpRequest(); // new HttpRequest instance
     xhttp4.onreadystatechange = function () {
         let needed = 15000;
@@ -56,6 +10,7 @@
         let remaining = 0;
         if (this.readyState == 4 && this.status == 200) {
             let obj = JSON.parse(this.responseText);
+            // console.log(obj);
             done = obj[0].todaysteps;
 
             remaining = needed - done;
@@ -64,7 +19,7 @@
             }
         }
         var ctx = document.getElementById("doughutSteps");
-        ctx.height = 250;
+        ctx.height = 200;
         var myChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
@@ -86,7 +41,8 @@
                 ]
             },
             options: {
-                responsive: true
+                responsive: true,
+                maintainAspectRatio: true
             }
         });
 
@@ -95,34 +51,56 @@
     xhttp4.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp4.send(localStorage.getItem('user'));
 
+//Kreslenie goals completed. Ked pojde api nakopirujem tu ten isty graf z daily goals
+//     var ctx = document.getElementById("doughutGoals");
+//     ctx.height = 250;
+//     var myChart = new Chart(ctx, {
+//         type: 'doughnut',
+//         data: {
+//             datasets: [{
+//                 data: [2, 4],
+//                 backgroundColor: [
+//                     "rgba(0, 194, 146,0.9)",
+//                     "rgba(194, 0, 0,0.7)",
+//                 ],
+//                 hoverBackgroundColor: [
+//                     "rgba(0, 194, 146,0.7)",
+//                     "rgba(194, 0, 0,0.7)",
+//                 ]
+//
+//             }],
+//             labels: [
+//                 "Completed goals",
+//                 "Remaining goals"
+//             ]
+//         },
+//         options: {
+//             responsive: true
+//         }
+//     });
 
-    //nakreslenie grafu pre splnenie goalu prejdenych minut
-    let req2 = "http://itsovy.sk:1203/getusertodayminutes";
-    var xhttp5 = new XMLHttpRequest(); // new HttpRequest instance
-    xhttp5.onreadystatechange = function () {
-        console.log("in");
-        console.log("status: " + this.status);
-        let needed = 1000;
+
+    //nakreslenie grafu pre splnenie goalu minut
+    var xhttpMinutes = new XMLHttpRequest(); // new HttpRequest instance
+    xhttpMinutes.onreadystatechange = function () {
+        let needed = 300;
         let done = 0;
+        let remaining = 0;
         if (this.readyState == 4 && this.status == 200) {
             let obj = JSON.parse(this.responseText);
-            // console.log(obj);
-            let jsonData = 0;
-            for (var i = 0; i < obj.length; i++) {
-                var obj2 = obj[i];
-                jsonData += obj2.thisSessionSteps;
-                // console.log(obj2.thisSessionSteps);
+            done = obj.minutes;
+            remaining = needed - done;
+            if (remaining < 0) {
+                remaining = 0;
             }
-            done = jsonData;
-
         }
         var ctx = document.getElementById("doughutMinutes");
-        ctx.height = 250;
+        ctx.height = 200;
         var myChart = new Chart(ctx, {
             type: 'doughnut',
             data: {
                 datasets: [{
-                    data: [done, needed - done],
+                    data: [done, remaining],
                     backgroundColor: [
                         "rgba(0, 194, 146,0.9)",
                         "rgba(194, 0, 0,0.7)",
@@ -134,8 +112,8 @@
 
                 }],
                 labels: [
-                    "Minutes done",
-                    "Minutes remaining"
+                    "Completed minutes",
+                    "Remaining minutes"
                 ]
             },
             options: {
@@ -144,8 +122,119 @@
         });
 
     };
-    xhttp5.open("POST", encodeURI(req2), "/json-handler");
-    xhttp5.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp5.send(localStorage.getItem('user'));
+    xhttpMinutes.open("POST", encodeURI("http://itsovy.sk:1203/getusertodayminutes"), "/json-handler");
+    xhttpMinutes.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttpMinutes.send(localStorage.getItem('user'));
 
+    //nakreslenie grafu pre splnenie goalu minut
+    var xhttpGoals = new XMLHttpRequest(); // new HttpRequest instance
+    xhttpGoals.onreadystatechange = function () {
+        let neededMinutes = 300;
+        let doneMinutes = 0;
+        let remainingMinutes = 0;
+        let neededSteps = 15000;
+        let doneSteps = 0;
+        let remainingSteps = 0;
+        if (this.readyState == 4 && this.status == 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            doneMinutes = obj.minutes;
+            console.log("doneMinutes: " + doneMinutes);
+            remainingMinutes = neededMinutes - doneMinutes;
+            if (remainingMinutes < 0) {
+                remainingMinutes = 0;
+            }
+            let req = "http://itsovy.sk:1203/getsteps";
+            var xhttpSteps = new XMLHttpRequest(); // new HttpRequest instance
+            xhttpSteps.onreadystatechange = function () {
+
+                if (this.readyState == 4 && this.status == 200) {
+                    let obj = JSON.parse(this.responseText);
+                    console.log(obj);
+                    doneSteps = obj[0].todaysteps;
+
+                    remainingSteps = neededSteps - doneSteps;
+                    if (remainingSteps < 0) {
+                        remainingSteps = 0;
+                    }
+                    console.log(doneSteps);
+                }
+
+                let goalsNeedToBeCompleted = 2;
+                let goalsCompleted = 0;
+                if (neededMinutes <= doneMinutes) {
+                    goalsCompleted++;
+                }
+                if (neededSteps <= doneSteps) {
+                    goalsCompleted++;
+                }
+                let remainingGoals = goalsNeedToBeCompleted - goalsCompleted;
+
+                var ctx = document.getElementById("doughutGoals");
+                ctx.height = 250;
+                var myChart = new Chart(ctx, {
+                    type: 'doughnut',
+                    data: {
+                        datasets: [{
+                            data: [goalsCompleted, remainingGoals],
+                            backgroundColor: [
+                                "rgba(0, 194, 146,0.9)",
+                                "rgba(194, 0, 0,0.7)",
+                            ],
+                            hoverBackgroundColor: [
+                                "rgba(0, 194, 146,0.7)",
+                                "rgba(194, 0, 0,0.7)",
+                            ]
+
+                        }],
+                        labels: [
+                            "Completed goals",
+                            "Remaining goals"
+
+                        ]
+                    },
+                    options: {
+                        responsive: true
+                    }
+                });
+
+
+            };
+            xhttpSteps.open("POST", encodeURI(req), "/json-handler");
+            xhttpSteps.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhttpSteps.send(localStorage.getItem('user'));
+        } else {
+            var ctx = document.getElementById("doughutGoals");
+            ctx.height = 250;
+            var myChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    datasets: [{
+                        data: [0, 2],
+                        backgroundColor: [
+                            "rgba(0, 194, 146,0.9)",
+                            "rgba(194, 0, 0,0.7)",
+                        ],
+                        hoverBackgroundColor: [
+                            "rgba(0, 194, 146,0.7)",
+                            "rgba(194, 0, 0,0.7)",
+                        ]
+
+                    }],
+                    labels: [
+                        "Completed goals",
+                        "Remaining goals"
+
+                    ]
+                },
+                options: {
+                    responsive: true
+                }
+            });
+        }
+    };
+
+    xhttpGoals.open("POST", encodeURI("http://itsovy.sk:1203/getusertodayminutes"), "/json-handler");
+    xhttpGoals.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttpGoals.send(localStorage.getItem('user'));
 })(jQuery);
